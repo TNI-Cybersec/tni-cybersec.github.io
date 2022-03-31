5872e0c8d73d00b4bf670614ccd1c9c559b776d8
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-04-01 05:16:04 +0700

    Implement component

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   // Load components
   $("#navigation").load("common/navbar.html");
   $("#footer").load("common/footer.html");
+  $("#cookie-banner").load("common/cookie-banner.html");
 });
 
 // Loader starts
