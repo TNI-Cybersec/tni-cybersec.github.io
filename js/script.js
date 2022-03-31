a6c9057892b9c575f743fa5dd8ede0b5f63c9b73
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-04-01 04:40:10 +0700

    Fix some bugs and refactor code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,30 @@ $(document).ready(function () {
   $("#navigation").load("common/navbar.html");
   $("#footer").load("common/footer.html");
 });
+
+// Loader starts
+
+var loader;
+
+function loadNow(opacity) {
+  if (opacity <= 0) {
+    displayContent();
+  } else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function () {
+      loadNow(opacity - 0.05);
+    }, 50);
+  }
+}
+
+function displayContent() {
+  loader.style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  loader = document.getElementById("loader");
+  loadNow(1);
+});
+
+// Loader ends
