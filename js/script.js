dd8bdca0c95808ac9fd7ae7633f5473b9255e1f5
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2022-04-04 03:58:48 +0700

    Big fix in 4.0.2

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ $(document).ready(function () {
   // Load components
   $("#navigation").load("common/navbar.html");
   $("#footer").load("common/footer.html");
+  $("#cookie-banner").load("common/cookie-banner.html");
+  $("#msg-plugin").load("common/messenger-plugin.html");
 });
