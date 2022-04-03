756429b6a780e3e6f36fc9d5645ee260b3d1b106
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2022-04-04 01:14:11 +0700

    Update 4.0.2

--- js/script.js
+++ js/script.js
@@ -2,33 +2,4 @@ $(document).ready(function () {
   // Load components
   $("#navigation").load("common/navbar.html");
   $("#footer").load("common/footer.html");
-  $("#cookie-banner").load("common/cookie-banner.html");
-  $("#msg-plugin").load("common/messenger-plugin.html");
 });
-
-// Loader starts
-
-var loader;
-
-function loadNow(opacity) {
-  if (opacity <= 0) {
-    displayContent();
-  } else {
-    loader.style.opacity = opacity;
-    window.setTimeout(function () {
-      loadNow(opacity - 0.05);
-    }, 50);
-  }
-}
-
-function displayContent() {
-  loader.style.display = "none";
-  document.getElementById("content").style.display = "block";
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  loader = document.getElementById("loader");
-  loadNow(1);
-});
-
-// Loader ends
