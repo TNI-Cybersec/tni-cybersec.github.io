54dfb236f6676f2cb3cb5b7ccb55a7ba8456cd17
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-08-07 04:23:51 +0700

    Feat: Create offline fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = "tni-cyber-site-v1";
+const OFFLINE_URL = "offline.html";
 const assets = [
   "/",
   "/index.html",
@@ -6,23 +7,57 @@ const assets = [
   "/activities.html",
   "/cybersec.html",
   "/termsofservice.html",
+  "/404.html",
+  "/offline.html",
   "/css/style.css",
   "/css/animate.min.css",
   "/js/app.js",
+  "/js/script.js",
+  "/js/carousel.js",
+  "/js/cookie.js",
+  "/js/version.js",
+  "/js/t.min.js",
+  "/js/wow.min.js",
 ];
 
 self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       cache.addAll(assets);
+      cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
     })
   );
 });
 
-self.addEventListener("fetch", (fetchEvent) => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then((res) => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  // Only call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          // First, try to use the navigation preload response if it's
+          // supported.
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          // Always try the network first.
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          // catch is only triggered if an exception is thrown, which is
+          // likely due to a network error.
+          // If fetch() returns a valid HTTP response with a response code in
+          // the 4xx or 5xx range, the catch() will NOT be called.
+          console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
 });
