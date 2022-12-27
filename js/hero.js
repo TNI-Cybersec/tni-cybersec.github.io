bfa7826d52c66edc3fed09698a9f5e547bf4a8f1
Karinzaa! <minkarin447@gmail.com>
2022-12-27 11:28:12 +0700

    Fix Hero.js

--- js/hero.js
+++ js/hero.js
@@ -1,9 +1,9 @@
-//Wellcome Screen
-var status1 = "🎄HPNY 2023!🎄"; 
+// Wellcome Screen
+var status1 = "🎄HPNY 2023!🎄";
 document.getElementById("status1").textContent = status1;
 var status2 = "🎄幸福な新年 2023!🎄";
 document.getElementById("status2").textContent = status2;
-//Below Wellcome Screen
+// Below Wellcome Screen
 var words = ["共に未来を前進させる", "クリエイティブは世界を作る", "未来は作れる", "すべての考えは常に重要です", "未来はここにある"];
 var word = words[Math.floor(Math.random() * words.length)];
 document.getElementById("hero").textContent = word;
